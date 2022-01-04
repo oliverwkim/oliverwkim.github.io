ad5aa18b6cb6b576e9c7bc1f7504070f8ae2a99b
oliverwkim <oliverwkim@gmail.com>
2022-01-04 11:09:48 -0800

    Update mountain_to_climb.js

--- assets/mountain_to_climb/mountain_to_climb.js
+++ assets/mountain_to_climb/mountain_to_climb.js
@@ -51,6 +51,7 @@ var svg = d3.select("#forecasts")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
+    .style("margin-left", "-120px")
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
